3a4b14e51ae010948f148e2df6f30d832cfaa639
Dan Wiebe <dnwiebe@gmail.com>
2020-01-31 07:07:42 -0500

    GH-241: All unit tests uncommented and working (except the #ignored one about concurrency)

--- masq/src/main.rs
+++ masq/src/main.rs
@@ -92,82 +92,86 @@ impl Main {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::test_utils::mocks::{MockCommand, CommandFactoryMock, CommandProcessorMock, CommandProcessorFactoryMock};
+    use crate::test_utils::mocks::{MockCommand, CommandFactoryMock, CommandProcessorMock, CommandProcessorFactoryMock, CommandContextMock};
     use std::sync::{Arc, Mutex};
-    use masq_lib::test_utils::fake_stream_holder::FakeStreamHolder;
+    use masq_lib::test_utils::fake_stream_holder::{FakeStreamHolder};
     use crate::commands::CommandError::Transaction;
     use masq_lib::messages::UiShutdownOrder;
+    use masq_lib::ui_gateway::NodeFromUiMessage;
+    use masq_lib::messages::ToMessageBody;
 
-//    #[test]
-//    fn go_works_when_everything_is_copacetic() {
-//        let port = find_free_port();
-//        let command = MockCommand::new (UiShutdownOrder{})
-//            .execute_result (Ok(()));
-//        let c_make_params_arc = Arc::new (Mutex::new (vec![]));
-//        let command_factory = CommandFactoryMock::new()
-//            .make_params(&c_make_params_arc)
-//            .make_result(Ok(Box::new (command)));
-//        let process_params_arc = Arc::new (Mutex::new (vec![]));
-//        let processor = CommandProcessorMock::new()
-//            .process_params (&process_params_arc)
-//            .process_result(Ok(()));
-//        let p_make_params_arc = Arc::new (Mutex::new(vec![]));
-//        let processor_factory = CommandProcessorFactoryMock::new()
-//            .make_params (&p_make_params_arc)
-//            .make_result (Box::new (processor));
-//        let mut subject = Main {
-//            command_factory: Box::new(command_factory),
-//            processor_factory: Box::new (processor_factory),
-//        };
-//
-//        let result = subject.go(&mut FakeStreamHolder::new().streams(), &[
-//            "command".to_string(),
-//            "--param1".to_string(),
-//            "value1".to_string(),
-//            "--param2".to_string(),
-//            "value2".to_string(),
-//            "subcommand".to_string(),
-//            "--param3".to_string(),
-//            "value3".to_string(),
-//            "param4".to_string(),
-//            "param5".to_string(),
-//        ]);
-//
-//        assert_eq! (result, 0);
-//        let c_make_params = c_make_params_arc.lock().unwrap();
-//        assert_eq! (*c_make_params, vec![
-//            vec!["subcommand".to_string(), "--param3".to_string(), "value3".to_string(),
-//                "param4".to_string(), "param5".to_string()],
-//        ]);
-//        let p_make_params = p_make_params_arc.lock().unwrap();
-//        assert_eq! (*p_make_params, vec![vec![
-//            "command".to_string(),
-//            "--param1".to_string(),
-//            "value1".to_string(),
-//            "--param2".to_string(),
-//            "value2".to_string(),
-//            "subcommand".to_string(),
-//            "--param3".to_string(),
-//            "value3".to_string(),
-//            "param4".to_string(),
-//            "param5".to_string(),
-//        ]]);
-//        let mut process_params = process_params_arc.lock().unwrap();
-//        let command = process_params.remove (0);
-//        let stream_holder_arc = Arc::new (Mutex::new (FakeStreamHolder::new()));
-//        let stream_holder_arc_inner = stream_holder_arc.clone();
-//        let result = {
-//            let mut stream_holder = stream_holder_arc_inner.lock().unwrap();
-//            let mut context = CommandContext::new(port);
-//            context.streams = stream_holder.streams();
-//            command.execute(&mut context)
-//        };
-//
-//        assert_eq! (result, Err(CommandError::Transaction("Booga!".to_string())));
-//        let stream_holder = stream_holder_arc.lock().unwrap();
-//        assert_eq! (stream_holder.stdout.get_string(), "MockCommand output\n".to_string());
-//        assert_eq! (stream_holder.stderr.get_string(), "MockCommand error\n".to_string());
-//    }
+    #[test]
+    fn go_works_when_everything_is_copacetic() {
+        let command = MockCommand::new (UiShutdownOrder{});
+        let c_make_params_arc = Arc::new (Mutex::new (vec![]));
+        let command_factory = CommandFactoryMock::new()
+            .make_params(&c_make_params_arc)
+            .make_result(Ok(Box::new (command)));
+        let process_params_arc = Arc::new (Mutex::new (vec![]));
+        let processor = CommandProcessorMock::new()
+            .process_params (&process_params_arc)
+            .process_result(Ok(()));
+        let p_make_params_arc = Arc::new (Mutex::new(vec![]));
+        let processor_factory = CommandProcessorFactoryMock::new()
+            .make_params (&p_make_params_arc)
+            .make_result (Box::new (processor));
+        let mut subject = Main {
+            command_factory: Box::new(command_factory),
+            processor_factory: Box::new (processor_factory),
+        };
+
+        let result = subject.go(&mut FakeStreamHolder::new().streams(), &[
+            "command".to_string(),
+            "--param1".to_string(),
+            "value1".to_string(),
+            "--param2".to_string(),
+            "value2".to_string(),
+            "subcommand".to_string(),
+            "--param3".to_string(),
+            "value3".to_string(),
+            "param4".to_string(),
+            "param5".to_string(),
+        ]);
+
+        assert_eq! (result, 0);
+        let c_make_params = c_make_params_arc.lock().unwrap();
+        assert_eq! (*c_make_params, vec![
+            vec!["subcommand".to_string(), "--param3".to_string(), "value3".to_string(),
+                "param4".to_string(), "param5".to_string()],
+        ]);
+        let p_make_params = p_make_params_arc.lock().unwrap();
+        assert_eq! (*p_make_params, vec![vec![
+            "command".to_string(),
+            "--param1".to_string(),
+            "value1".to_string(),
+            "--param2".to_string(),
+            "value2".to_string(),
+            "subcommand".to_string(),
+            "--param3".to_string(),
+            "value3".to_string(),
+            "param4".to_string(),
+            "param5".to_string(),
+        ]]);
+        let mut process_params = process_params_arc.lock().unwrap();
+        let command = process_params.remove (0);
+        let transact_params_arc = Arc::new (Mutex::new (vec![]));
+        let mut context = CommandContextMock::new()
+            .transact_params (&transact_params_arc)
+            .transact_result (Err("not really an error".to_string()));
+        let stdout_arc = context.stdout_arc();
+        let stderr_arc = context.stderr_arc();
+
+        let result = command.execute(&mut context);
+
+        assert_eq! (result, Err(Transaction("not really an error".to_string())));
+        let transact_params = transact_params_arc.lock().unwrap();
+        assert_eq! (*transact_params, vec![NodeFromUiMessage {
+            client_id: 0,
+            body: UiShutdownOrder{}.tmb(1),
+        }]);
+        assert_eq! (stdout_arc.lock().unwrap().get_string(), "MockCommand output".to_string());
+        assert_eq! (stderr_arc.lock().unwrap().get_string(), "MockCommand error".to_string());
+    }
 
     #[test]
     fn go_works_when_given_no_subcommand() {
--- masq/src/test_utils/mocks.rs
+++ masq/src/test_utils/mocks.rs
@@ -3,16 +3,17 @@
 use std::sync::{Mutex, Arc};
 use std::cell::RefCell;
 use crate::command_factory::{CommandFactoryError, CommandFactory};
-use masq_lib::ui_gateway::{NodeFromUiMessage};
+use masq_lib::ui_gateway::{NodeFromUiMessage, NodeToUiMessage};
 use masq_lib::messages::{UiShutdownOrder, UiSetup};
 use lazy_static::lazy_static;
 use masq_lib::messages::ToMessageBody;
 use crate::commands::{CommandError, Command};
 use crate::command_context::{CommandContext};
-//use crate::command_context::{CommandContextFactory, CommandContextFactoryError};
 use crate::commands::CommandError::Transaction;
 use crate::command_processor::{CommandProcessor, CommandProcessorFactory};
 use crate::websockets_client::nfum;
+use std::io::{Read, Write};
+use masq_lib::test_utils::fake_stream_holder::{ByteArrayWriterInner, ByteArrayWriter};
 
 lazy_static! {
     pub static ref ONE_WAY_MESSAGE: NodeFromUiMessage = NodeFromUiMessage {
@@ -53,6 +54,77 @@ impl CommandFactoryMock {
     pub fn make_result(self, result: Result<Box<dyn Command>, CommandFactoryError>) -> Self {
         self.make_results.borrow_mut().push (result);
         self
+    }
+}
+
+pub struct CommandContextMock {
+    transact_params: Arc<Mutex<Vec<NodeFromUiMessage>>>,
+    transact_results: RefCell<Vec<Result<NodeToUiMessage, String>>>,
+    stdout: Box<dyn Write>,
+    stdout_arc: Arc<Mutex<ByteArrayWriterInner>>,
+    stderr: Box<dyn Write>,
+    stderr_arc: Arc<Mutex<ByteArrayWriterInner>>,
+}
+
+impl CommandContext for CommandContextMock {
+    fn send(&mut self, _message: NodeFromUiMessage) -> Result<(), String> {
+        unimplemented!()
+    }
+
+    fn transact(&mut self, message: NodeFromUiMessage) -> Result<NodeToUiMessage, String> {
+        self.transact_params.lock().unwrap().push (message);
+        self.transact_results.borrow_mut ().remove (0)
+    }
+
+    fn stdin(&mut self) -> &mut Box<dyn Read> {
+        unimplemented!()
+    }
+
+    fn stdout(&mut self) -> &mut Box<dyn Write> {
+        &mut self.stdout
+    }
+
+    fn stderr(&mut self) -> &mut Box<dyn Write> {
+        &mut self.stderr
+    }
+
+    fn close(&mut self) {
+        unimplemented!()
+    }
+}
+
+impl CommandContextMock {
+    pub fn new () -> Self {
+        let stdout = ByteArrayWriter::new();
+        let stdout_arc = stdout.inner_arc();
+        let stderr = ByteArrayWriter::new();
+        let stderr_arc = stderr.inner_arc();
+        Self {
+            transact_params: Arc::new (Mutex::new (vec![])),
+            transact_results: RefCell::new (vec![]),
+            stdout: Box::new (stdout),
+            stdout_arc,
+            stderr: Box::new (stderr),
+            stderr_arc,
+        }
+    }
+
+    pub fn transact_params(mut self, params: &Arc<Mutex<Vec<NodeFromUiMessage>>>) -> Self {
+        self.transact_params = params.clone();
+        self
+    }
+
+    pub fn transact_result(self, result: Result<NodeToUiMessage, String>) -> Self {
+        self.transact_results.borrow_mut().push (result);
+        self
+    }
+
+    pub fn stdout_arc(&self) -> Arc<Mutex<ByteArrayWriterInner>> {
+        self.stdout_arc.clone()
+    }
+
+    pub fn stderr_arc(&self) -> Arc<Mutex<ByteArrayWriterInner>> {
+        self.stderr_arc.clone()
     }
 }
 
@@ -142,14 +214,12 @@ impl<T: ToMessageBody + Clone> std::fmt::Debug for MockCommand<T> {
 
 impl<T: ToMessageBody + Clone> Command for MockCommand<T> {
     fn execute(&self, context: &mut dyn CommandContext) -> Result<(), CommandError> {
-        let result = context.transact(nfum(self.message.clone()));
-        match result {
-            Ok(_) => (),
-            Err(e) => return Err(Transaction(e)),
-        }
         write!(context.stdout(), "MockCommand output").unwrap();
         write!(context.stderr(), "MockCommand error").unwrap();
-        self.execute_results.borrow_mut().remove (0)
+        match context.transact(nfum(self.message.clone())) {
+            Ok(_) => self.execute_results.borrow_mut().remove (0),
+            Err(e) => Err(Transaction(e)),
+        }
     }
 }
 
