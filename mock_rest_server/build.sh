ac6581b5d02ba21c2f0a2476f506442904e4b7f2
masqrauder <60554948+masqrauder@users.noreply.github.com>
2020-03-21 18:57:42 -0400

    Fix path to bash.

--- mock_rest_server/build.sh
+++ mock_rest_server/build.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash -evx
+#!/bin/bash -evx
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
 [[ $GITHUB_ACTIONS -eq true ]] && sudo --preserve-env ../node/ci/free-port-53.sh
