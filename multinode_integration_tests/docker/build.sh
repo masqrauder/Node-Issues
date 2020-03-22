5e7a22a9eb9e39f76d2d11fa4d3c2ba86a86d6b3
masqrauder <60554948+masqrauder@users.noreply.github.com>
2020-03-21 23:12:43 -0400

    Fix path to bash.

--- multinode_integration_tests/docker/build.sh
+++ multinode_integration_tests/docker/build.sh
@@ -2,13 +2,14 @@
 # Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
 function ensure_dns_works_with_github_actions() {
-  echo "running ensure_dns_works_with_github_actions"
+  echo "Running ensure_dns_works_with_github_actions"
   sudo tee /etc/docker/daemon.json << 'EOF'
   {
     "cgroup-parent": "/actions_job",
     "dns": ["1.1.1.1", "8.8.8.8"]
   }
 EOF
+  echo "Restarting docker service"
   sudo service docker restart
   sleep 10
 }
@@ -17,6 +18,6 @@ mkdir -p generated
 
 cp ../../port_exposer/target/debug/port_exposer generated/port_exposer
 
-[[ $GITHUB_ACTIONS -eq true ]] && sudo --preserve-env ../../node/ci/free-port-53.sh
-[[ $GITHUB_ACTIONS -eq true ]] && ensure_dns_works_with_github_actions
+sudo --preserve-env ../../node/ci/free-port-53.sh
+ensure_dns_works_with_github_actions
 docker build -t test_node_image .
--- node/ci/free-port-53.sh
+++ node/ci/free-port-53.sh
@@ -2,4 +2,4 @@
 systemctl disable systemd-resolved.service
 service systemd-resolved stop
 # [[ -e /etc/resolv.conf ]] && cat /etc/resolv.conf
-[[ -e /etc/resolv.conf ]] && sed -i 's/127.0.0.53/1.1.1.1/g' /etc/resolv.conf
+[[ -f /etc/resolv.conf ]] && sed -i 's/127.0.0.53/1.1.1.1/g' /etc/resolv.conf
