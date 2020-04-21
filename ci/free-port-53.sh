3ae5dfb38d3943ffdda17b3745eed9cd4a5da8cb
masqrauder <60554948+masqrauder@users.noreply.github.com>
2020-04-21 00:41:38 -0400

    Fix errors in free-port-53.sh.

--- ci/free-port-53.sh
+++ ci/free-port-53.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -evx
-command -v systemctrl && systemctl disable systemd-resolved.service
+command -v systemctl && systemctl stop systemd-resolved
+command -v systemctl && systemctl disable systemd-resolved.service
 command -v service && service systemd-resolved stop
 # [[ -e /etc/resolv.conf ]] && cat /etc/resolv.conf
-[[ -f /etc/systemd/resolv.conf ]] && sed -E -e -E -e "s/#DNS=$/DNS=1.1.1.1/g
-s/#DNSStubListener=yes$/DNSStubListener=no/g" -i '' /etc/systemd/resolv.conf
-[[ -f /etc/systemd/resolve/resolv.conf && -L /etc/resolv.conf ]] && ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+[[ -f /etc/systemd/resolved.conf ]] && sed -E -e 's/#DNS=$/DNS=1.1.1.1/g' -e 's/#DNSStubListener=yes$/DNSStubListener=no/g' -i '' /etc/systemd/resolved.conf || echo 'Edit Complete'
+[[ -f /run/systemd/resolve/resolv.conf && -L /etc/resolv.conf ]] && ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
