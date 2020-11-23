10f28fb58321e37638b83850d002edaef370b832
masqrauder <60554948+masqrauder@users.noreply.github.com>
2020-11-23 00:10:31 -0500

    Change CARGO_TARGET_DIR

--- ci/all.sh
+++ ci/all.sh
@@ -7,7 +7,7 @@ ci/format.sh
 
 # Remove these two lines to slow down the build
 which sccache || cargo install sccache || echo "Skipping sccache installation"  # Should do significant work only once
-export CARGO_TARGET_DIR=$HOME/.cargo/cargo-cache/
+export CARGO_TARGET_DIR="$CI_DIR/../cargo-cache"
 export SCCACHE_DIR="$HOME/.cargo/sccache"
 #export RUSTC_WRAPPER="$HOME/.cargo/bin/sccache"
 SCCACHE_IDLE_TIMEOUT=0 sccache --start-server || echo "sccache server already running"
